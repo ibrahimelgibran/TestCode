e4d5414ec50e5b076acd08a0b7e87309bd0b99f1
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-08-21 22:57:40 +0700

    first commit

---
+++ src/main/java/internship_msib/interview_code/InterviewCodeApplication.java
@@ -0,0 +1,13 @@
+package internship_msib.interview_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewCodeApplication.class, args);
+	}
+
+}
